1ca67ac90bb386da94a149ad55c1823efd0e668b
Santiago Cameto <santiago.cameto@estudiantes.utec.edu.uy>
2024-09-18 15:52:49 -0300

    add model empresa

--- backend/empresa/models.py
+++ backend/empresa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Empresa(models):
+    nombre = models.CharField(max_length=255)
+    rut = models.CharField(max_length=12)
+    direccion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+    
