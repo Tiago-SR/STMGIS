b37753c7eac437c16a4ced945c9ba55cc0858b59
Santiago Gaston Cameto Hernandez <santiago.cameto@estudiantes.utec.edu.uy>
2024-09-27 20:38:47 -0300

    cambiosVariosCampo

--- backend/ambientes/models.py
+++ backend/ambientes/models.py
@@ -3,10 +3,26 @@
 from django.contrib.gis.db import models as geomodels
 from django.db import models
 
-class Ambiente(models.Model):  
+
+class Ambiente(models.Model):     
+ 
     campo = models.ForeignKey('campo.Campo', related_name='ambientes', on_delete=models.CASCADE) 
-    datos = models.JSONField(null=True, blank=True)  
-    ambiente_geom = geomodels.MultiPolygonField(null=True, blank = True,srid=4326)  
+    #datos = models.JSONField(null=True, blank=True)  
+    ambiente_geom = geomodels.MultiPolygonField(null=True, blank = True,srid=4326) 
+    name = models.CharField(max_length=100, null=True, blank=True)
+    area = models.FloatField(null=True, blank=True)
+    ia = models.IntegerField(null=True, blank=True)
+    lote = models.CharField(max_length=100, null=True, blank=True)
+    sist_prod = models.CharField(max_length=100, null=True, blank=True)
+    zona = models.CharField(max_length=100, null=True, blank=True)
+    tipo_suelo = models.IntegerField(null=True, blank=True)
+    posicion = models.CharField(max_length=100, null=True, blank=True)
+    prof = models.CharField(max_length=100, null=True, blank=True)
+    restriccion = models.CharField(max_length=100, null=True, blank=True)
+    ambiente = models.CharField(max_length=100, null=True, blank=True)
+    idA = models.IntegerField( null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+     
 
     def __str__(self):        
         nombre_ambiente = self.datos.get('Name', 'Sin Nombre')  
--- backend/campo/models.py
+++ backend/campo/models.py
@@ -2,18 +2,25 @@ from django.db import models
 from django.contrib.gis.db import models as gis_models
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.geos import Polygon
-
+import uuid
 
 
 from empresa.models import Empresa
 # Create your models here.
 class Campo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nombre = models.CharField(max_length=100)
     superficie = models.FloatField()   
     departamento = models.CharField(max_length=100)
     empresa = models.ForeignKey(Empresa, related_name='campos', on_delete=models.CASCADE)
     shapePoligon = gis_models.PolygonField(null=True, blank=True, srid=4326)  # Campo espacial
     is_active = models.BooleanField(default=True)
+
+    def delete(self, *args, **kwargs):
+        self.is_active = False  # Marcar el campo como inactivo en lugar de eliminarlo
+        self.save()
+        # Aplicar el soft delete a los objetos Ambiente relacionados
+        self.ambientes.update(is_active=False)
 
     def __str__(self):
         return self.nombre
